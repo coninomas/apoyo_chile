b03efb670507dbffe4d7f39b319fb7de9223acf5
Coni Rivera <coniriveragis@gmail.com>
2024-03-08 19:18:02 -0300

    Al hacer click en los titulos de las tarjetas, desaparecerá el texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ console.log
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+$( "h3" ).on( "click", function() {
+  $( "#card-text1" ).toggle("slow");
+  $( "#card-text2" ).toggle("slow");
+  $( "#card-text3" ).toggle("slow");
+  $( "#card-text4" ).toggle("slow");
+});
